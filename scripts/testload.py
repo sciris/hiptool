a39d03536d65ebe8ba721fe3e5489746325ef5c7
cliffckerr <questionably@gmail.com>
2018-02-27 01:58:02 +0200

    this is better

--- scripts/testload.py
+++ scripts/testload.py
@@ -1,3 +1,3 @@
-from hpptool import Project
-P = Project(interventionsfile='../data/dcp-data.xlsx')
+from hpptool import Project, HPpath
+P = Project(interventionsfile=HPpath('data')+'dcp-data.xlsx')
 print('\n\nExample entry:\n\n%s' % P.intersets[0].data[37])
