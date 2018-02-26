8c7b76dbea05496ff8da82ffff3612a8e6801f71
cliffckerr <questionably@gmail.com>
2018-02-26 23:54:21 +0200

    made bin files

---
+++ bin/start_dev_server.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+# Load Sciris
+from sciris.api import app
+
+# Run the server
+app.run()
---
+++ bin/start_server.py
@@ -0,0 +1,8 @@
+#!/usr/env/python
+
+# Load Sciris
+from sciris import _autoreload
+from sciris import _twisted_wsgi
+
+# Run the server
+_autoreload.main(_twisted_wsgi.run)
