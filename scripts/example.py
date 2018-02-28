08053c4126afcc09ffa730b308a8607b4ba665ac
cliffckerr <questionably@gmail.com>
2018-02-28 15:35:45 +0200

    started on example, just copying for now

---
+++ scripts/example.py
@@ -0,0 +1,14 @@
+"""
+This script demonstrates example usage of HealthPrior.
+
+Version: 2018feb28
+"""
+
+from hptool import Project, HPpath
+
+dp = HPpath('data')
+P = Project(burdenfile=dp+'ihme-gbd.xlsx', interventionsfile=dp+'dcp-data.xlsx')
+
+print('\n\nExample burden entry:\n\n%s' % P.burdensets[0].data[37])
+
+print('\n\nExample interventions entry:\n\n%s' % P.intersets[0].data[37])
