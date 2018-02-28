382d6418a021643d9470d804af6a4c9fe328b14b
cliffckerr <questionably@gmail.com>
2018-02-28 18:28:00 +0200

    try this?

--- webapp/config.py
+++ webapp/config.py
@@ -8,39 +8,43 @@
 # you use a relative path, it is interpreted as being with respect to the 
 # root path of the main repository.
 
-# Last update: 12/21/17 (gchadder3)
-
-# A secret key value used by Python Flask.
-SECRET_KEY = 'Pick something unique for your site here'
+# Last update: 2018-02-28
 
 ROOT_DIR = '/u/cliffk/unsw/hptool/'
 
+config = {
+
+# A secret key value used by Python Flask.
+'SECRET_KEY': 'Pick something unique for your site here',
+
+
 # Directory containing the client code.
-CLIENT_DIR = ROOT_DIR+'client'
+'CLIENT_DIR': ROOT_DIR+'client',
 
 # Directory containing the model code.  This is the primary Python code that 
 # the web app will rely on related to the actual model.
-MODEL_DIR = ROOT_DIR+'hptool'
+'MODEL_DIR': ROOT_DIR+'hptool',
 
 # Directory containing Python code specific to the web app.  sessionmanager 
 # (the directory this config file is in) contains general Python functionality 
 # that the web app directory code makes use of. 
-WEBAPP_DIR = ROOT_DIR+'webapp'
+'WEBAPP_DIR': ROOT_DIR+'webapp',
 
 # Directory where uploaded files will be saved, as well as files saved to be 
 # downloadable to user machines.
-UPLOADS_DIR = ROOT_DIR+'uploads'
+'UPLOADS_DIR': ROOT_DIR+'uploads',
 
 # Directory where files may be saved that the web app should be able to 
 # access.
-FILESAVEROOT_DIR = ROOT_DIR+'savedfiles'
+'FILESAVEROOT_DIR': ROOT_DIR+'savedfiles',
 
 # URL for the Redis database that the web app will use to manage 
 # persistence.  Note that the /N/ number at the end should match the 
 # database number you want to use.  (N=0 is the default Redis database.)
-REDIS_URL = 'redis://localhost:6379/5/'
+'REDIS_URL': 'redis://localhost:6379/5/',
 
 # Flag for setting whether registration of a new account automatically 
 # spawns a new active account.  If this is set False, then an admin user has 
 # to manually activate the account for the user.
-REGISTER_AUTOACTIVATE = True
+'REGISTER_AUTOACTIVATE': True,
+}
--- webapp/start_server.py
+++ webapp/start_server.py
@@ -2,7 +2,7 @@
 
 # Load Sciris
 from sciris import server
-from ..webapp import config
+from config import config
 
 # Run the server
 #server.start()
