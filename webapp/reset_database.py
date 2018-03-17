33f930b3985247bbbea1d90fcb2f620b40d67504
cliffckerr <questionably@gmail.com>
2018-03-17 14:24:05 +0100

    added script to reset database

---
+++ webapp/reset_database.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+'''
+Small script to reset the user database -- WARNING, deletes everything!!!
+
+Version: 2018mar17
+'''
+
+import sciris.datastore as ds
+import config
+
+answer = raw_input('Are you sure you want to reset the database for "%s"? (y/[n]): ' % (config.ROOT_DIR))
+if answer == 'y':
+	ds.theDataStore = ds.DataStore(redisDbURL=config.REDIS_URL)
+	ds.theDataStore.deleteAll()
+	print('Database reset.')
+else:
+	print('Database not reset.')
