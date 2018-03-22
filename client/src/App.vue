4d90f5b383a4b4c9a7cf1bc7a0691eabdff06457
James Jansson <jamesjansson@gmail.com>
2018-03-22 21:45:18 +1100

    Adds sidebar and menu bar to app.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -5,16 +5,48 @@ Last update: 2/2/18 (gchadder3)
 -->
 
 <template>
-  <div id="app">
-    <!-- The top bar, including app title and menu -->
-    <top-bar></top-bar>
-
-    <!-- Switchable view -->
+  <div :class="{'nav-open': $sidebar.showSidebar}">
     <router-view></router-view>
+    <!--This sidebar appears only for screens smaller than 992px-->
+    <side-bar type="navbar" :sidebar-links="$sidebar.sidebarLinks">
+      <ul class="nav navbar-nav">
+        <li>
+          <a class="dropdown-toggle" data-toggle="dropdown">
+            <i class="ti-panel"></i>
+            <p>Stats</p>
+          </a>
+        </li>
+        <drop-down title="5 Notifications" icon="ti-bell">
+
+          <li><a>Notification 1</a></li>
+          <li><a>Notification 2</a></li>
+          <li><a>Notification 3</a></li>
+          <li><a>Notification 4</a></li>
+          <li><a>Another notification</a></li>
+
+        </drop-down>
+        <li>
+          <a>
+            <i class="ti-settings"></i>
+            <p>Settings</p>
+          </a>
+        </li>
+        <li class="divider"></li>
+      </ul>
+    </side-bar>
+    <!-- <div id="app">
+      The top bar, including app title and menu
+      <top-bar></top-bar>
+
+      Switchable view
+      <router-view></router-view>
+    </div> -->
   </div>
+  
 </template>
 
 <script>
+export default {}
 import TopBar from '@/components/TopBar'
 
 export default {
