c0f3552e7aa3fd9ed4d21c266a38677af50c8e31
cliffckerr <questionably@gmail.com>
2018-03-23 22:31:03 +0100

    removed sidebar stuff

--- client/src/components/generic/SidebarPlugin/index.js
+++ client/src/components/generic/SidebarPlugin/index.js
@@ -32,41 +32,6 @@ const SidebarStore = {
       name: 'Change password',
       icon: 'ti-shield',
       path: '/admin/changepassword'
-    },
-    {
-      name: 'Dashboard',
-      icon: 'ti-panel',
-      path: '/admin/overview'
-    },
-    {
-      name: 'User Profile',
-      icon: 'ti-user',
-      path: '/admin/stats'
-    },
-    {
-      name: 'Table List',
-      icon: 'ti-view-list-alt',
-      path: '/admin/table-list'
-    },
-    {
-      name: 'Typography',
-      icon: 'ti-text',
-      path: '/admin/typography'
-    },
-    {
-      name: 'Icons',
-      icon: 'ti-pencil-alt2',
-      path: '/admin/icons'
-    },
-    {
-      name: 'Maps',
-      icon: 'ti-map',
-      path: '/admin/maps'
-    },
-    {
-      name: 'Notifications',
-      icon: 'ti-bell',
-      path: '/admin/notifications'
     }
   ],
   displaySidebar (value) {
