6a54899d191928ed932b52b2d970c036fed9574e
cliffckerr <questionably@gmail.com>
2018-03-24 13:48:19 +0100

    added logos

--- client/src/components/app/About.vue
+++ client/src/components/app/About.vue
@@ -6,7 +6,15 @@ Last update: 3/7/18 (gchadder3)
 
 <template>
   <div class="SitePage">
-  	<p>The Health Services Prioritization Tool was developed in partnership by University College London and the Burnet Institute under the auspices of the Bill and Melinda Gates Foundation and the World Bank.</p>
+    <div style="max-width:800px; margin:auto">
+      <img src="static/img/healthpriorlogo.png" width="250px">
+      <br/><br/>
+      <p>The Health Services Prioritization Tool is currently under development. Development is supported through a partnership
+        between <a href="http://www.ucl.ac.uk">University College London</a> and the <a href="http://www.burnet.edu.au">Burnet Institute</a>,
+        under the auspices of the <a href="http://www.gatesfoundation.org">Bill and Melinda Gates Foundation</a> and the <a href="http://www.worldbank.org">World Bank</a>.</p>
+
+      <p></p>
+    </div>
   </div>
 </template>
 
