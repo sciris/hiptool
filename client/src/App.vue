13459498cd3a7e8d9d856a2db25d276347fe4d94
James Jansson <jamesjansson@gmail.com>
2018-03-24 19:34:13 +1100

    Fix collapse view

--- client/src/App.vue
+++ client/src/App.vue
@@ -11,19 +11,19 @@ Last update: 2/2/18 (gchadder3)
     <side-bar type="navbar" :sidebar-links="$sidebar.sidebarLinks">
       <ul class="nav navbar-nav">
         <!-- Below requires a userService -->
-        <!-- <li>
+        <li>
           <a href="#" class="btn-rotate">
             <i class="ti-view-grid"></i>
             <p>
               <b>Project: </b><span>{{ activeProjectName }}</span>
             </p>
           </a>
-        </li> -->
-        <!-- <drop-down v-bind:title="currentUser.username" icon="ti-user">
+        </li>
+        <drop-down v-bind:title="currentUser.username" icon="ti-user">
           <li><a href="#/admin/changeinfo"><i class="ti-pencil"></i>&nbsp;Edit account</a></li>
           <li><a href="#/admin/changepassword"><i class="ti-shield"></i>&nbsp;Change password</a></li>
           <li><a href="#"><i class="ti-car"></i>&nbsp;Log out</a></li>
-        </drop-down> -->
+        </drop-down>
         <li class="divider"></li>
       </ul>
     </side-bar>
@@ -32,7 +32,25 @@ Last update: 2/2/18 (gchadder3)
 </template>
 
 <script>
-export default {}
+import userService from '@/services/user-service'
+
+export default {
+  computed: {
+    // Health prior function
+    currentUser: () => {
+      return userService.currentUser()
+    },
+
+    activeProjectName() {
+      if (this.$store.state.activeProject.project === undefined) {
+        return 'none'
+      } else {
+        return this.$store.state.activeProject.project.name
+      }
+    },
+  }
+
+}
 
 </script>
 
