1728762e38afe9803c83ba7f6055f9faf7d40b20
cliffckerr <questionably@gmail.com>
2018-03-26 01:03:48 +0200

    registration page is nice

--- client/src/components/app/RegisterPage.vue
+++ client/src/components/app/RegisterPage.vue
@@ -4,41 +4,75 @@ User registration page
 Last update: 2018mar25
 -->
 
-<template>
-  <div class="SitePage" style="width: 500px; margin:auto">
 
-    <div class="divTable">
-      <div class="divTableBody">
-        <div class="divTableRow">
-          <div class="divRowLabel">Username: </div>
-          <div class="divRowContent"><input v-model='registerUserName'/></div>
-        </div>
-        <div class="divTableRow">
-          <div class="divRowLabel">Password:</div>
-          <div class="divRowContent"><input v-model='registerPassword'/></div>
-        </div>
-        <div class="divTableRow">
-          <div class="divRowLabel">Display name: </div>
-          <div class="divRowContent"><input v-model='registerDisplayName'/></div>
-        </div>
-        <div class="divTableRow">
-          <div class="divRowLabel">Email: </div>
-          <div class="divRowContent"><input v-model='registerEmail'/></div>
+<template>
+  <div class="SitePage" style="background-color:#f8f8f4; position:fixed; min-height:100%; min-width:100%; padding:0 0 0 0" v-model="getVersionInfo"> <!-- Should match _variables.scss:$bg-nude -->
+    <div style="background-color:#212120; position:absolute; height:100%; width:260px">
+      <div class="logo">
+        <div class="simple-text" style="font-size:20px; color:#fff; font-weight:bold; padding:20px">
+          <div class="logo-img" style="height:40px; width:40px; line-height:40px; border-radius:40px; background-color:#fff; text-align:center; display:inline-block">
+            <img src="static/favicon-96x96.png" width="21px" vertical-align="middle" alt>
+          </div>
+          <span style="padding-left:10px">
+            <img src="static/img/healthpriorlogo-inverse.png" width="130px" vertical-align="middle" alt>
+          </span>
+          <br/><br/>
+          <div style="font-size:14px; font-weight:normal">
+            Beta version {{ version }} ({{ date }})
+          </div>
         </div>
       </div>
     </div>
+    <div style="margin-right:-260px">
+      <form name="RegisterForm" @submit.prevent="tryRegister" style="max-width: 500px; min-width: 100px; margin: 0 auto">
 
-    <button class="btn __green" @click="tryRegister">Register</button>
-    <br/>
+        <div class="modal-body">
+          <h2>Register</h2>
 
-    <p v-if="registerResult != ''">{{ registerResult }}</p>
+          <div class="section" v-if="loginResult != ''">{{ loginResult }}</div>
 
-    Already have an account?
-    <router-link to="/login">
-        Log in
-    </router-link>
+          <div class="section form-input-validate">
+            <input class="txbox __l"
+                   type="text"
+                   name="username"
+                   placeholder="User name"
+                   required="required"
+                   v-model='registerUserName'/>
+          </div>
+
+          <div class="section form-input-validate">
+            <input class="txbox __l"
+                   type="password"
+                   name="password"
+                   placeholder="Password"
+                   required="required"
+                   v-model='registerPassword'/>
+          </div>
+
+          <div class="section form-input-validate">
+            <input class="txbox __l"
+                   type="text"
+                   name="displayname"
+                   placeholder="Display name (optional)"
+                   v-model='registerDisplayName'/>
+          </div>
+
+          <div class="section form-input-validate">
+            <input class="txbox __l"
+                   type="text"
+                   name="email"
+                   placeholder="Email (optional)"
+                   v-model='registerEmail'/>
+          </div>
+
+          <button type="submit" class="section btn __l __block">Register</button>
+
+        </div>
+      </form>
+    </div>
   </div>
 </template>
+
 
 <script>
 import rpcservice from '@/services/rpc-service'
