aaf4d3621f9f7869ac43858b387e572d7ef82f9a
cliffckerr <questionably@gmail.com>
2018-06-03 22:13:19 -0700

    clean up imports

--- hptool/webapp/main.py
+++ hptool/webapp/main.py
@@ -5,16 +5,15 @@ Last update: 6/1/18 (gchadder3)
 """
 
 # Imports
-from sciris.scirisapp import ScirisApp
+import sciris.web as sw
 import sciris.core as sc
-from . import config
-from . import project
 import hptool as hp
+import hptool.webapp as hpwa
 
 def make_app():
 	# Create the ScirisApp object.  NOTE: app.config will thereafter contain all 
 	# of the configuration parameters, including for Flask.
-	app = ScirisApp(__file__, app_config=config)
+	app = sw.ScirisApp(__file__, app_config=hpwa.config)
 
 	# RPC definitions
 	@app.register_RPC()
@@ -22,16 +21,16 @@ def make_app():
 		''' Return the information about the project. '''
 		gitinfo = sc.gitinfo(__file__)
 		version_info = {
-	        'version': hp.version,
-	        'date': hp.versiondate,
+	        'version':   hp.version,
+	        'date':      hp.versiondate,
 	        'gitbranch': gitinfo['branch'],
-	        'githash': gitinfo['hash'],
-	        'gitdate': gitinfo['date'],
+	        'githash':   gitinfo['hash'],
+	        'gitdate':   gitinfo['date'],
 		}
 		return version_info
 
 	# Register the RPCs in the project.py module.
-	app.add_RPC_dict(project.RPC_dict)
+	app.add_RPC_dict(hpwa.project.RPC_dict)
 
 	return app
 
@@ -42,7 +41,7 @@ def run():
 	app = make_app()
 	
 	# Initialize the projects.
-	project.init_projects(app)
+	hp.webapp.project.init_projects(app)
 
 	# Run the client page with Flask and a Twisted server.
 	app.run_server()
