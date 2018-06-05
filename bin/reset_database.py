b58443bae75793a571d26c42ade1a3b9edefae20
cliffckerr <questionably@gmail.com>
2018-06-04 20:14:59 -0700

    improved

--- bin/reset_database.py
+++ bin/reset_database.py
@@ -8,8 +8,12 @@ Version: 2018jun04
 
 import sciris.web as sw
 import hptool as hp
+import os
 
-answer = raw_input('Are you sure you want to reset the database for "%s"? (y/[n]): ' % (hp.webapp.config.CLIENT_DIR))
+webapp_dir = os.path.abspath(hp.webapp.config.CLIENT_DIR)
+redis_url = hp.webapp.config.REDIS_URL
+prompt = 'Are you sure you want to reset the database for the following?\n  %s\n  %s\nAnswer (y/[n]): ' % (webapp_dir, redis_url)
+answer = raw_input(prompt)
 if answer == 'y':
 	theDataStore = sw.DataStore(redis_db_URL=hp.webapp.config.REDIS_URL)
 	theDataStore.delete_all()
