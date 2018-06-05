6e830c6e27a00475f470e9f775f01942f07bdde6
cliffckerr <questionably@gmail.com>
2018-06-04 22:25:21 -0700

    minor fix

--- bin/reset_database.py
+++ bin/reset_database.py
@@ -15,7 +15,7 @@ redis_url = hp.webapp.config.REDIS_URL
 prompt = 'Are you sure you want to reset the database for the following?\n  %s\n  %s\nAnswer (y/[n]): ' % (webapp_dir, redis_url)
 answer = raw_input(prompt)
 if answer == 'y':
-	theDataStore = sw.DataStore(redis_db_URL=hp.webapp.config.REDIS_URL)
+	theDataStore = sw.DataStore(redis_db_URL=redis_url)
 	theDataStore.delete_all()
 	print('Database reset.')
 else:
