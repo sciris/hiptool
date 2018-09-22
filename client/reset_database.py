acdd988cfbdbd562566175056f4e19138147f5d3
cliffckerr <questionably@gmail.com>
2018-09-22 17:16:08 +1000

    merged client folder

--- client/reset_database.py
+++ client/reset_database.py
@@ -3,7 +3,7 @@
 '''
 Small script to reset the user database -- WARNING, deletes everything!!!
 
-Version: 2018jun04
+Version: 2018sep22
 '''
 
 import scirisweb as sw
@@ -12,11 +12,10 @@ import os
 
 webapp_dir = os.path.abspath(hp.webapp.config.CLIENT_DIR)
 redis_url = hp.webapp.config.REDIS_URL
+datastore = sw.DataStore(redis_url=redis_url)
 prompt = 'Are you sure you want to reset the database for the following?\n  %s\n  %s\nAnswer (y/[n]): ' % (webapp_dir, redis_url)
 answer = raw_input(prompt)
 if answer == 'y':
-	theDataStore = sw.DataStore(redis_db_URL=redis_url)
-	theDataStore.delete_all()
-	print('Database reset.')
+	datastore.flushdb()
 else:
 	print('Database not reset.')
