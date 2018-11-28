525dc0481025caf2b9edfdbea748a9f54b3aa162
Parham Saidi <parham@parha.me>
2018-11-28 16:32:11 +0100

    flat folder structure

--- resetdb.py
+++ resetdb.py
@@ -1,20 +1,21 @@
-#!/usr/bin/env python
-
 '''
 Small script to reset the user database -- WARNING, deletes everything!!!
 
-Version: 2018sep22
+Version: 2018jun04
 '''
 
 import scirisweb as sw
 import hptool as hp
 import os
 
+try:    inputfunc = raw_input
+except: inputfunc = input
+
 webapp_dir = os.path.abspath(hp.webapp.config.CLIENT_DIR)
 redis_url = hp.webapp.config.REDIS_URL
 datastore = sw.DataStore(redis_url=redis_url)
 prompt = 'Are you sure you want to reset the database for the following?\n  %s\n  %s\nAnswer (y/[n]): ' % (webapp_dir, redis_url)
-answer = raw_input(prompt)
+answer = inputfunc(prompt)
 if answer == 'y':
 	datastore.flushdb()
 else:
