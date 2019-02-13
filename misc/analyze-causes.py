e5b7ff2e0e72ae38089e3b49730b4fd2822bf629
cliffckerr <questionably@gmail.com>
2019-02-13 18:56:38 +1100

    hmm

---
+++ misc/analyze-causes.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 13 18:51:43 2019
+
+@author: cliffk
+"""
+
+import hptool as hp
+
+causedict = hp.causedict
+codes = causedict.values()
+sorco = sorted(codes)
