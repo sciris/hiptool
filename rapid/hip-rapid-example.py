86f0b59b292ad7568918640eb5a831dcf6171e1b
cliffckerr <questionably@gmail.com>
2019-10-22 16:21:53 -0700

    renamed

--- rapid/hip-rapid-example.py
+++ rapid/hip-rapid-example.py
@@ -5,8 +5,8 @@ import hiptool as hp
 doplot = False
 
 P = hp.Project()
-P.loadburden(filename='demo_BoD.xlsx')
-P.loadinterventions(filename='CIV_interventions.xlsx')
+P.loadburden(filename='rapid_BoD.xlsx')
+P.loadinterventions(filename='rapid_interventions.xlsx')
 P.intervsets[0].data['Spending'] *= 0 # Set current spending to 0
 ninterventions = P.intervsets[0].data.nrows
 P.makepackage()
