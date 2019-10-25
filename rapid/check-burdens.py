2fa26c6c8e15aaced7a907811f285e1dbbe14c09
cliffckerr <questionably@gmail.com>
2019-10-24 21:33:43 -0700

    data matches

--- rapid/check-burdens.py
+++ rapid/check-burdens.py
@@ -2,4 +2,13 @@ import sciris as sc
 
 bod = sc.loadobj('/home/cliffk/unsw/hiptool/data/gbd-data.dat')
 
+countrydata = sc.loadspreadsheet('country-data.xlsx')
+
+bod_countries = set(bod.keys())
+data_countries = set(countrydata['name'].tolist())
+
+print(f'Matching:\n{data_countries.intersection(bod_countries)}\n')
+print(f'Extra:\n{data_countries-bod_countries}\n')
+print(f'Missing:\n{bod_countries-data_countries}\n')
+
 print('Done.')
